fcfc61e492c45183069cc345bb437f460929ffe8
supendi <irpan.supendi@gmail.com>
2021-04-20 15:20:03 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Account, AccountGetRequest, AccountUpdateRequest, LoginRequest, Registrant } from './account'
+import { Account, AccountGetRequest, AccountUpdateRequest, Registrant } from './account'
 import { Profile, Album, Personil, ProfileRow, Song, Video } from './profile'
+import { LoginRequest } from './auth'
 
 export type { Profile, Album, Personil, ProfileRow, Song, Video }
-export type { Account, AccountGetRequest, AccountUpdateRequest, LoginRequest, Registrant }
+export type { Account, AccountGetRequest, AccountUpdateRequest, Registrant }
+export type { LoginRequest }
 
