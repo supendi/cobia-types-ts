126ec125df4015e5a5201b4a86786397748f9775
supendi <irpan.supendi@gmail.com>
2021-05-03 08:13:32 +0700

    release

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import { Account, AccountGetRequest, AccountUpdateRequest, Registrant } from './account';
+import { Profile, Album, Personil, ProfileRow, Song, Video } from './profile';
+import { LoginRequest } from './auth';
+import { UpdateViewRequest } from './notification';
+export type { Profile, Album, Personil, ProfileRow, Song, Video };
+export type { Account, AccountGetRequest, AccountUpdateRequest, Registrant };
+export type { LoginRequest };
+export type { UpdateViewRequest };
---
+++ dist/notification/index.d.ts
@@ -0,0 +1,3 @@
+export interface UpdateViewRequest {
+    offsets: number[];
+}
